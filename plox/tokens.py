cddd379a7de7e12354eba996fafa1b0c8cb21ef6
caleb <caleb.ball@explain.com.au>
2021-08-09 22:33:04 +1000

    Add basic token class and token enum
    We're mostly just defining that big enum here. It translates pretty
    easily, though I think we could do some fun enum shenanigans (I suspect
    that will be one of the first things I come back to, because I'm already
    itching for it.)

---
+++ plox/tokens.py
@@ -0,0 +1,64 @@
+import enum
+
+from attr import define
+
+
+class TokenType(enum.Enum):
+    # Single character tokens
+    LEFT_PAREN = enum.auto()
+    RIGHT_PAREN = enum.auto()
+    LEFT_BRACE = enum.auto()
+    RIGHT_BRACE = enum.auto()
+    COMMA = enum.auto()
+    DOT = enum.auto()
+    MINUS = enum.auto()
+    PLUS = enum.auto()
+    SEMICOLON = enum.auto()
+    SLASH = enum.auto()
+    STAR = enum.auto()
+
+    # One or two character tokens
+    BANG = enum.auto()
+    BANG_EQUAL = enum.auto()
+    EQUAL = enum.auto()
+    EQUAL_EQUAL = enum.auto()
+    GREATER = enum.auto()
+    GREATER_EQUAL = enum.auto()
+    LESS = enum.auto()
+    LESS_EQUAL = enum.auto()
+
+    # Literals
+    IDENTIFIER = enum.auto()
+    STRING = enum.auto()
+    NUMBER = enum.auto()
+
+    # Keywords
+    AND = enum.auto()
+    CLASS = enum.auto()
+    ELSE = enum.auto()
+    FALSE = enum.auto()
+    FUN = enum.auto()
+    FOR = enum.auto()
+    IT = enum.auto()
+    NIL = enum.auto()
+    OR = enum.auto()
+    PRINT = enum.auto()
+    RETURN = enum.auto()
+    SUPER = enum.auto()
+    THIS = enum.auto()
+    TRUE = enum.auto()
+    VAR = enum.auto()
+    WHILE = enum.auto()
+
+    EOF = enum.auto()
+
+
+@define
+class Token:
+    type: TokenType
+    lexeme: str
+    literal: object
+    line: int
+
+    def __str__(self):
+        return f"{self.type} {self.lexeme} {self.literal}"
