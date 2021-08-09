abc8515b7641bc70db28f37fa7e99933a47ad244
caleb <caleb.ball@explain.com.au>
2021-08-09 23:49:22 +1000

    Refactor main Plox class
    The initial Lox class in the book is a singleton object. I had (by habit
    I guess) setup my version of this class such that it requires
    instantiation, which makes some things not really work (like testing.)
    So in this change I've made everything a class method/attribute, which
    fixes these problems.
    
    I guess I could have gotten it to work by creating an instance in
    `plox.cli`, but I think this will match the book's pattern better.

--- plox/cli.py
+++ plox/cli.py
@@ -2,53 +2,56 @@ import readline
 import sys
 from typing import List
 
-from attr import define
 
-from plox.scanner import Scanner
-
-
-@define
 class Plox:
-    had_error: bool = False
+    HAD_ERROR: bool = False
 
-    def main(self, args: List[str]):
+    @classmethod
+    def main(cls, args: List[str]):
         if len(args) > 1:
             print("Usage: plox [script]")
             sys.exit(64)
 
         if len(args) == 1:
-            self.run_file(args[0])
+            cls.run_file(args[0])
 
-        self.run_prompt()
+        cls.run_prompt()
 
-    def run_file(self, path: str):
+    @classmethod
+    def run_file(cls, path: str):
         with open(path, "r") as source:
-            self.run(source.read())
+            cls.run(source.read())
 
-        if self.had_error:
+        if cls.HAD_ERROR:
             sys.exit(65)
 
-    def run_prompt(self):
+    @classmethod
+    def run_prompt(cls):
         try:
             while True:
                 line = input("> ")
-                self.run(line)
-                self.had_error = False
+                cls.run(line)
+                cls.HAD_ERROR = False
         except EOFError:
             pass
 
-    def run(self, source: str):
+    @classmethod
+    def run(cls, source: str):
+        from plox.scanner import Scanner
+
         scanner = Scanner(source)
         tokens = scanner.scan_tokens()
         print(tokens)
 
-    def error(self, line: int, message: str):
-        self.report(line, "", message)
+    @classmethod
+    def error(cls, line: int, message: str):
+        cls.report(line, "", message)
 
-    def report(self, line: int, where: str, message: str):
+    @classmethod
+    def report(cls, line: int, where: str, message: str):
         print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
-        self.had_error = True
+        cls.HAD_ERROR = True
 
 
 def main():
-    Plox().main(sys.argv[1:])
+    Plox.main(sys.argv[1:])
