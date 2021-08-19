8e7bb36ae2551ec56b21697ddb657f912f173fbf
caleb <caleb.ball@explain.com.au>
2021-08-19 16:29:22 +1000

    Add a new AST class tree for statements
    And here is the results of the previously updated `generateAst` script.
    This is (for the most part) just some "dumb" data classes, so I haven't
    added any tests (which I also didn't do for the original expression
    classes.)

---
+++ plox/statements.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import 
+
+from attr import define
+
+from plox.tokens import Token
+
+
+class Stmt:
+    def accept(self, visitor: "StmtVisitor"):
+        ...
+
+
+@define
+class Expression(Stmt):
+    expression: Expr
+
+    def accept(self, visitor: "StmtVisitor"):
+        return visitor.visit_expression(self)
+
+
+@define
+class Print(Stmt):
+    expression: Expr
+
+    def accept(self, visitor: "StmtVisitor"):
+        return visitor.visit_print(self)
+
+
+class StmtVisitor(ABC):
+    @abstractmethod
+    def visit_expression(self, expr: Expression):
+        ...
+
+    @abstractmethod
+    def visit_print(self, expr: Print):
+        ...
